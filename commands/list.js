08d868430b40367ebad9449a4062edfb38dab762
Aras Uludag <arasuludag@gmail.com>
2026-02-02 22:39:32 +0300

    fix: list command chracter limits

--- commands/list.js
+++ commands/list.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -16,10 +16,52 @@ module.exports = {
       memberList = memberList.concat(`${role.user.toString()}\n`);
     });
 
-    interaction.reply({
-      content: `${mentionedRole.toString()} has 
-${memberList}`,
+    const EMBED_DESC_LIMIT = 4096;
+
+    // If the list is short enough, send in one embed
+    if (memberList.length <= EMBED_DESC_LIMIT) {
+      const embed = new EmbedBuilder()
+        .setTitle(`${mentionedRole.name} Members (${mentionedRole.members.size})`)
+        .setDescription(memberList || "No members found")
+        .setColor(mentionedRole.color);
+
+      return interaction.reply({
+        embeds: [embed],
+        ephemeral: true,
+      });
+    }
+
+    // If too long, split into multiple messages
+    await interaction.reply({
+      content: `${mentionedRole.toString()} has ${mentionedRole.members.size} members. Sending list in multiple messages...`,
       ephemeral: true,
     });
+
+    const chunks = [];
+    let currentChunk = "";
+
+    mentionedRole.members.forEach((member) => {
+      const line = `${member.user.toString()}\n`;
+      if ((currentChunk + line).length > EMBED_DESC_LIMIT) {
+        chunks.push(currentChunk);
+        currentChunk = line;
+      } else {
+        currentChunk += line;
+      }
+    });
+
+    if (currentChunk) chunks.push(currentChunk);
+
+    for (let i = 0; i < chunks.length; i++) {
+      const embed = new EmbedBuilder()
+        .setTitle(`${mentionedRole.name} Members (Part ${i + 1}/${chunks.length})`)
+        .setDescription(chunks[i])
+        .setColor(mentionedRole.color);
+
+      await interaction.followUp({
+        embeds: [embed],
+        ephemeral: true,
+      });
+    }
   },
 };
